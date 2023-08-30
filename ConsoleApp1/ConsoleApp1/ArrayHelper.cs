ac83ab8eb4f0d114d2cd062fe13ff612a086cc92
Predator <murad.mammedzade11@gmail.com>
2023-08-30 05:22:09 -0700

    Save

---
+++ ConsoleApp1/ConsoleApp1/ArrayHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1
+{
+    public class ArrayHelper
+    {
+        public static void Add(ref string[] array1, string a)
+        {
+            Array.Resize(ref array1, array1.Length + 1);
+            array1[array1.Length - 1] = a;
+            foreach (string s in array1)
+            {
+                Console.WriteLine(s);
+            }
+        }
+        public static void Remove(ref string[] arr, string a)
+        {
+            arr = arr.Where(o => o != a).ToArray();
+            foreach (string s in arr)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = { "1", "2", "3", "4", "5" };
+            string a = "6";
+            ArrayHelper.Add(ref array1, a);
+            ArrayHelper.Remove(ref array1, array1[0]);
+        }
+    }
+}
